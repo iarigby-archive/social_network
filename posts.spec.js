ef1464537359ed1934ae0b61642a45bc6554c0e2
iarigby <marvinzem@gmail.com>
2020-01-14 21:17:42 +0400

    fix up tests

--- posts.spec.js
+++ posts.spec.js
@@ -6,18 +6,18 @@ function getElements(className) {
     return Array.from(document.getElementsByClassName(className))
 }
 
-function delay(interval, id) {
-    return it(`დალოდება ${id}`, done => {
-        setTimeout(() => done(), interval)
-    }).timeout(interval + 100) // The extra 100ms should guarantee the test will not fail due to exceeded timeout
-}
+// function delay(interval, id) {
+//     return it(`დალოდება ${id}`, done => {
+//             setTimeout(() => done(), interval)
+//         }).timeout(interval + 100) // The extra 100ms should guarantee the test will not fail due to exceeded timeout
+// }
 
 function checkExists(elemID, nodeType) {
     const element = document.getElementById(elemID)
     expect(element).to.not.be.null
     if (nodeType)
         console.log('todo')
-    //check   
+        //check   
 }
 describe('პოსტები', () => {
     const postTextClass = 'post-text'
@@ -32,24 +32,25 @@ describe('პოსტები', () => {
         var submitID = 'submit-post'
         it('პოსტის გამოქვეყნებისთვის უნდა იყოს ღილაკი id-ით ' + submitID, () => {
             checkExists(submitID)
+                // ბექენდთან კავშირის დასამყარებლად
+                // setTimeout(() => done(), 500)
         })
         const postClass = 'post'
-        // todo wait for condition
-        delay(500, 'ბექენდთან კავშირის დასამყარებლად')
-        it(`ტექსტის შეყვანის და ღილაკზე დაჭერის შემოწმება`, () => {
+        it(`ტექსტის შეყვანის და ღილაკზე დაჭერის შემოწმება`, (done) => {
             getElement(newPostID).value = postText
             const e = document.querySelector(`button#${submitID}`)
             const click = new MouseEvent('click', {
                 view: window
             })
             e.dispatchEvent(click)
+                // ახალი პოსტის გამოსაჩენად
+            setTimeout(() => done(), 300)
         })
-        delay(300, 'ახალი პოსტის გამოსაჩენად')
         it(`ახალი პოსტის ტექსტი, შექმნის შემდეგ,\
     უნდა დაემატოს ${postTextClass} კლასის ელემენტად სხვა ასეთ \
     ელემენტებამდე, ხოლო შეყვანილი ტექსტი უნდა გასუფთავდეს `, () => {
             const post = document.querySelector(`div.${postTextClass}`)
-            // console.log(post)
+                // console.log(post)
             expect(post.innerHTML.trim()).to.equal(postText)
             expect(getElement(newPostID).value == '')
         })
@@ -59,14 +60,17 @@ describe('პოსტები', () => {
     describe('წაშლა', () => {
         // TODO check that I'm deleting og post
         const deleteClass = 'delete-post'
-        delay(900, 'პოსტის წაშლამდე')
         it(`პოსტის წაშლა უნდა შეიძლებოდეს ${deleteClass}\
-        კლასის ღილაკით`, () => {
-            const remove = document.querySelector(`button.${deleteClass}`)
-            expect(remove).to.not.be.null
-            remove.click()
-        })
-        delay(200, 'პოსტის წაშლისთვის')
+        კლასის ღილაკით`, (done) => {
+                const remove = document.querySelector(`button.${deleteClass}`)
+                expect(remove).to.not.be.null
+                const click = new MouseEvent('click', {
+                    view: window
+                })
+                remove.dispatchEvent(click)
+                setTimeout(() => done(), 300)
+            })
+            // delay(200, 'პოსტის წაშლისთვის')
         it('პოსტის წაშლის შემოწმება', () => {
             const text = document.querySelector(`div.${postTextClass}`).innerHTML
             expect(text).to.not.equal(postText)
